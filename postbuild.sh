9afa8a836bd7b29a346e3d585b9f756213733d20
Morgyn <morgyn@gmail.com>
2024-09-20 22:05:33 +0100

    Postbuild script

---
+++ postbuild.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+mkdir -p site
+while read -r l; do
+	if [[ \"${l}\" =~ '/* index.css */' ]]; then
+		cat src/index.css
+	elif [[ \"${l}\" =~ '<!-- main.js -->' ]]; then
+		cat dist/main.js
+	else
+		echo \"${l}\"
+	fi
+done < src/index.html >site/index.html
